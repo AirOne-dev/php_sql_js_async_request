76ccaaa2fe9e3f47f41fddac597e727b85671168
Firo <31392527+Firologame@users.noreply.github.com>
2020-09-14 10:46:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,19 +10,22 @@ function get_page_content(action) {
     else
         p = 1;
     
-    document.querySelector('#vehicle_container').innerHTML = "<img src='https://upload.wikimedia.org/wikipedia/commons/b/b9/Youtube_loading_symbol_1_(wobbly).gif'></img>";
-    // met un gif de chargement dans la div '#vehicle_container'
-    
-    fetch('./pages.php?p=' + p).then(function (response) {
-        return response.text();
-    }).then(function (html) {
-        var parser = new DOMParser();
-        var doc = parser.parseFromString(html, 'text/html');
-        var content = doc.querySelector("#vehicle_container").innerHTML;
+    setTimeout(function(){ 
+        document.querySelector('#vehicle_container').innerHTML = "<img src='https://upload.wikimedia.org/wikipedia/commons/b/b9/Youtube_loading_symbol_1_(wobbly).gif'></img>";
+        // met un gif de chargement dans la div '#vehicle_container'
+    }, 1000).then(function() {
+  
+        fetch('./pages.php?p=' + p).then(function (response) {
+            return response.text();
+        }).then(function (html) {
+            var parser = new DOMParser();
+            var doc = parser.parseFromString(html, 'text/html');
+            var content = doc.querySelector("#vehicle_container").innerHTML;
         
-        document.querySelector('#vehicle_container').innerHTML = content;
-    }).catch(function (err) {
-        console.warn('Something went wrong.', err);
+            document.querySelector('#vehicle_container').innerHTML = content;
+        }).catch(function (err) {
+            console.warn('Something went wrong.', err);
+        });
     });
 }
 
