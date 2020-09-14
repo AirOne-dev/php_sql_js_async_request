9e4ba239be25f33c950be74b65e189cd46239299
Firo <31392527+Firologame@users.noreply.github.com>
2020-09-14 10:29:08 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <link rel="stylesheet" href="style.css">
 
-<div id='vehicle_container'></div>
+<div id='vehicle_container'>
+  <?php include_once('pages.php'); ?>
+</div>
 
 <button id='prev'>Précédent</button>
 <button id='next'>Suivant</button><br />
